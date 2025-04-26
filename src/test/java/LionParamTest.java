407f00cdae0996235199bcbf860e732bc5e62f8b
ibogdankova <ir.bogdankova@yandex.ru>
2025-04-27 01:01:56 +0300

    add LionTest

---
+++ src/test/java/LionParamTest.java
@@ -0,0 +1,24 @@
+import com.example.Lion;
+import com.example.Predator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LionParamTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Самец", "Самка"})
+    void testDoesHaveMane(String sex) throws Exception {
+        Predator predator = Mockito.mock(Predator.class);
+        Lion lion = new Lion(sex, predator);
+
+        if ("Самец".equals(sex)) {
+            assertTrue(lion.doesHaveMane());
+        } else {
+            assertFalse(lion.doesHaveMane());
+        }
+    }
+}
