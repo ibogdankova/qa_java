948e3a0253709124263c1a8b22aed81bb37baa6e
ibogdankova <ir.bogdankova@yandex.ru>
2025-04-27 01:57:22 +0300

    add FelineTest

---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,31 @@
+import com.example.Feline;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FelineTest {
+
+    @Test
+    void testEatMeat() throws Exception {
+        Feline feline = new Feline();
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), feline.eatMeat());
+    }
+
+    @Test
+    void testGetFamily() {
+        Feline feline = new Feline();
+        assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    void testGetKittens() {
+        Feline feline = new Feline();
+        assertEquals(1, feline.getKittens());
+    }
+
+    @Test
+    void testGetKittensWithCount() {
+        Feline feline = new Feline();
+        assertEquals(3, feline.getKittens(3));
+    }
+}
